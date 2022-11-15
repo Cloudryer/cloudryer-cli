7a2205d062f2d5bd3e69c667cd7b2fb69fc2d795
ziv <whoisziv@gmail.com>
2022-11-14 20:20:30 -0800

    removing unsupported options. Fixing description

--- bin/index.js
+++ bin/index.js
@@ -3,21 +3,21 @@ const {Command} = require('commander');
 const program = new Command();
 
 program
-  .name('Cloudryer')
+  .name('cloudryer')
   .description('CLI to analyze AWS EC2 instances\' cost')
   .version('1.0.0');
 
 program.command('list-machines')
   .description('List EC2 Machines')
   // .argument('<string>', 'string to split')
-  .option('--hide-utilization', "Won't show current CPU/network/disk usage")
-  .option('-t, --show-tags', 'Show associated tags/labels for each machine')
+  // .option('--hide-utilization', "Won't show current CPU/network/disk usage")
+  // .option('-t, --show-tags', 'Show associated tags/labels for each machine')
   .option('-w, --calculate-waste', 'Calculate money waste over a period of time')
   .option('-p, --evaluation-period [number]', 'Set waste evaluation period in days', 90)
   .action(async (options) => {
     const {listMachines} = require('../machines/commands');
     await listMachines(options);
-    // const limit = options["first"] ? 1 : undefined;
+    
 
   });
 
